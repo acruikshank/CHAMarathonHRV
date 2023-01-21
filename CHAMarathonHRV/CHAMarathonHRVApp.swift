ef05a82c8e014f5177f5f287323090969e9a6688
acruikshank <acruikshank@users.noreply.github.com>
2023-01-21 14:09:56 -0500

    Initial Commit

---
+++ CHAMarathonHRV/CHAMarathonHRVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHAMarathonHRVApp.swift
+//  CHAMarathonHRV
+//
+//  Created by Alex Cruikshank on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CHAMarathonHRVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
